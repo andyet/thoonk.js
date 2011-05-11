e4943264755b4b9d47f13e892e68f3d1c37cd357
Nathan Fritz <nathan@andyet.net>
2011-05-10 20:51:30 -0700

    updated jobs to match contract

--- thoonk.js
+++ thoonk.js
@@ -390,12 +390,16 @@ function queue_get(timeout, callback, timeout_callback) {
             .hget("feed.items:" + this.name, id)
             .hdel("feed.items:" + this.name, id)
             .exec(function(err, result) {
-                callback(result, id);
+                callback(result[0], id);
             });
         } else {
             timeout_callback();
         }
     }.bind(this));
+}
+
+function queue_get_ids(callback) {
+    return this.mredis.lrange("feed.ids:" + this.name, 0, -1, callback);
 }
 
 Queue.super_ = Feed;
@@ -410,9 +414,11 @@ Queue.prototype.publish = queue_publish;
 Queue.prototype.put = queue_publish;
 Queue.prototype.get = queue_get;
 Queue.prototype.high_put = queue_publishfront;
+Queue.prototype.get_ids = queue_get_ids;
 
 function Job(thoonk, name, config) {
     Feed.call(this, thoonk, name, config, 'job');
+    this.publish = this.thoonk.lock.require(this.publish, this);
 }
 
 Job.super_ = Queue;
@@ -423,45 +429,64 @@ Job.prototype = Object.create(Queue.prototype, {
     }
 });
 
+function job_publish(item, callback, high_priority) {
+    var id = uuid();
+    var multi = this.mredis.multi();
+    if(high_priority === true) {
+        multi.rpush("feed.ids:" + this.name, id);
+    } else {
+        multi.lpush("feed.ids:" + this.name, id);
+    }
+    multi.incr("feed.publishes:" + this.name);
+    multi.hset("feed.items:" + this.name, id, item);
+    multi.zadd("feed.published:" + this.name, Date.now(), id);
+    multi.exec(function(err, reply) {
+        this.thoonk.lock.unlock();
+    }.bind(this));
+}
+
 function job_get(timeout, callback, timeout_callback) {
     if(!timeout) timeout = 0;
     this.bredis.brpop("feed.ids:" + this.name, timeout, function(err, result) {
         if(!err) {
-            var d = new Date();
             var id = result[1];
-            this.mredis.hset("feed.running:" + this.name, id, d.getTime());
-            this.mredis.hget("feed.items:" + this.name, id, function(err, result) {
+            this.mredis.multi()
+                .zadd("feed.claimed:" + this.name, Date.now(), result[1])
+                .hget("feed.items:" + this.name, result[1])
+            .exec(function(err, result) {
                 callback(result, id);
             }.bind(this));
+            var d = new Date();
         } else {
             timeout_callback();
         }
     }.bind(this));
 }
 
-function job_finish(id, setresult, callback, error_callback) {
-    this.mredis.hdel("feed.running:" + this.name, id, function(err, result) {
-        if(!err) {
-            if(setresult !== null && setresult !== undefined) {
-                this.mredis.hget("feed.items:" + this.name, id, function(err, result) {
-                    if(!err) {
-                        this.mredis.lpush("feed.jobfinished:" + this.name + "\x00" + id, setresult);
-                    }
-                });
+function job_finish(id, callback, error_callback, result) {
+    this.mredis.watch("feed.claimed:" + this.name);
+    this.mredis.zrank("feed.claimed:" + this.name, id, function(err, result) {
+        if(result == null) {
+            error_callback(id);
+        } else {
+            var multi = this.mredis.multi();
+            multi.zrem("feed.claimed:" + this.name, id);
+            multi.hdel("feed.cancelled:" + this.name, id);
+            if(setresult) {
+                multi.lpush("feed.jobfinished:" + this.name + "\x00" + id, setresult);
+                multi.expire("feed.jobfinished:" + this.name + "\x00" + id);
             }
-            this.mredis.hdel("feed.items:" + this.name, id, function(err, result) {
-                if(err) {
-                    if(error_callback) {
-                        error_callback(id);
-                    }
+            multi.hdel("feed.items:" + this.name, id);
+            multi.exec(function(err, reply) {
+                if(reply == null) {
+                    //watch failed, try again
+                    process.nextTick(function() {
+                        this.job_finish(id, callback, error_callback, result);
+                    }.bind(this));
                 } else {
-                    if(callback) {
-                        callback(id);
-                    }
+                    callback(id);
                 }
             }.bind(this));
-        } else {
-            error_callback(id);
         }
     }.bind(this));
 }
@@ -471,33 +496,94 @@ function job_get_result(id, timeout, callback, timeout_callback) {
         if(err) {
             timeout_callback(id);
         } else {
-            callback(result);
+            callback(id);
         }
     }.bind(this));
 }
 
-function job_cancel(id) {
-    this.mredis.hdel("feed.running:" + this.name, id, function(err, result) {
-        if(!err) {
-            this.mredis.rpush("feed.ids:" + this.name, id);
+function job_cancel(id, callback) {
+    this.mredis.watch("feed.claimed:" + this.name);
+    this.mredis.zrank("feed.claimed:" + this.name, id, function(err, result) {
+        if(result == null) {
+            if(callback !== undefined) {
+                callback("id unclaimed", id);
+            }
+        } else {
+            this.mredis.multi()
+                .hincrby("feed.cancelled:" + this.name, id, 1)
+                .lpush("feed.ids:" + this.name, id)
+                .zrem("feed.claimed:" + this.name, id)
+            .exec(function(err, reply) {
+                if(reply == null) {
+                    //multi interrupted so retry
+                    process.nextTick(function() {
+                        this.job_cancel(id);
+                    }.bind(this));
+                } else {
+                    if(callback !== undefined) {
+                        callback(false, id);
+                    }
+                }
+            }.bind(this));
         }
-    });
+    }.bind(this));
 }
 
-function job_stall(id) {
-    this.mredis.hdel("feed.running:" + this.name, id, function(err, result) {
-        if(!err) {
-            this.mredis.rpush("feed.stalled:" + this.name, id);
+function job_stall(id, callback) {
+    this.mredis.watch("feed.claimed:" + this.name);
+    this.mredis.zrank("feed.claimed:" + this.name, id, function(err, result) {
+        if(result == null) {
+            if(callback !== undefined) {
+                callback("id unclaimed", id);
+            }
+        } else {
+            this.mredis.multi()
+                .zrem("feed.claimed:" + this.name, id)
+                .hdel("feed.cancelled:" + this.name, id)
+                .sadd("feed.stalled:" + this.name, id)
+                .zrem("feed.published:" + this.name, id)
+            .exec(function(err, reply) {
+                if(reply == null) {
+                    //multi interrupted so retry
+                    process.nextTick(function() {
+                        this.job_stall(id, callback);
+                    }.bind(this));
+                } else {
+                    if(callback !== undefined) {
+                        callback(false, id);
+                    }
+                }
+            }.bind(this));
         }
-    });
+    }.bind(this));
 }
 
-function job_retry(id) {
-    this.mredis.lrem("feed.stalled:" + this.name, 1, id, function(err, result) {
-        if(!err) {
-            this.mredis.rpush("feed.ids:" + this.name, id);
+function job_retry(id, callback) {
+    this.mredis.watch("feed.stalled:" + this.name);
+    this.mredis.sismember("feed.stalled:" + this.name, id, function(err, result) {
+        if(result == null) {
+            if(callback !== undefined) {
+                callback("id not stalled", id);
+            }
+        } else {
+            this.mredis.multi()
+                .srem("feed.stalled:" + this.name, id)
+                .lpush("feed.ids:" + this.name, id)
+                .zadd("feed.published:" + this.name, Date.now(), id)
+            .exec(function(err, reply) {
+                if(reply == null) {
+                    //multi interrupted so retry
+                    process.nextTick(function() {
+                        this.job_retry(id, callback);
+                    }.bind(this));
+                } else {
+                    if(callback !== undefined) {
+                        callback(false, id);
+                    }
+                }
+            }.bind(this));
         }
-    });
+    }.bind(this));
 }
 
 Job.prototype.get = job_get;
