c29971e93aae7897b0fdd058e38d863f7b53e925
Lance Stout <lancestout@gmail.com>
2011-06-16 13:27:48 -0700

    Added docs to sorted_feed.js

--- sorted_feed.js
+++ sorted_feed.js
@@ -5,6 +5,27 @@
 
 var Feed = require("./feed.js").Feed;
 
+/**
+ *
+ * A Thoonk sorted feed is a manually ordered collection of items.
+ * 
+ * Redis Keys Used:
+ *     feed.idincr:[feed]  -- A counter for ID values.
+ *     feed.publish:[feed] -- A channel for publishing item position
+ *                            change events.
+ * 
+ * Thoonk Standard API:
+ *     append    -- Append an item to the end of the feed.
+ *     edit      -- Edit an item in-place.
+ *     get_all   -- Return all items in the feed.
+ *     get_ids   -- Return the IDs of all items in the feed.
+ *     get_item  -- Return a single item from the feed given its ID.
+ *     prepend   -- Add an item to the beginning of the feed.
+ *     retract   -- Remove an item from the feed.
+ *     publish   -- Add an item to the end of the feed.
+ *     publish_after  -- Add an item immediately before an existing item.
+ *     publish_before -- Add an item immediately after an existing item.
+ */
 function SortedFeed(thoonk, name, config) {
     Feed.call(this, thoonk, name, config, 'sorted_feed');
     this.publish = this.thoonk.lock.require(sortedFeedPublish, this);
@@ -14,7 +35,19 @@ function SortedFeed(thoonk, name, config) {
     this.retract = this.thoonk.local.require(sortedFeedRetract, this);
 }
 
-//callback(item, id)
+/**
+ * Add an item to the feed.
+ *
+ * Arguments:
+ *     item     -- The contents of the item to publish.
+ *     callback -- Executed on successful publication.
+ *     prepend  -- If true, add the item to the start of the feed
+ *                 instead of the end.
+ *
+ * Callback Arguments:
+ *     item -- The contents of the published item.
+ *     id   -- The generated ID of the published item.
+ */
 function sortedFeedPublish(item, callback, prepend) {
     this.mredis.incr('feed.idincr:' + this.name, function(err, reply) {
         var id = reply;
@@ -38,11 +71,33 @@ function sortedFeedPublish(item, callback, prepend) {
     }.bind(this));
 }
 
+/**
+ * Add an item to the beginning of the feed.
+ *
+ * Arguments:
+ *     item     -- The contents of the item to publish.
+ *     callback -- Executed on successful publication.
+ *
+ * Callback Arguments:
+ *     item -- The contents of the published item.
+ *     id   -- The generated ID of the published item.
+ */
 function sortedFeedPrepend(item, callback) {
     this.publish(item, callback, true);
 }
 
-//callback(item, id)
+/**
+ * Modify an item in-place in the feed.
+ *
+ * Arguments:
+ *     id       -- The ID of the item to edit.
+ *     item     -- The new contents of the item.
+ *     callback -- Executed on successful modification.
+ *
+ * Callback Arguments:
+ *     item -- The new contents of the item.
+ *     id   -- The ID of the item.
+ */
 function sortedFeedEdit(id, item, callback) {
     this.mredis.watch('feed.items:' + this.name, function(err, reply) {
         this.mredis.hexists('feed.items:' + this.name, id, function(err, reply) {
@@ -71,15 +126,32 @@ function sortedFeedEdit(id, item, callback) {
     }.bind(this));
 }
 
-//callback(item, id);
-function sortedFeedPublishInsert(item, before_id, callback, placement) {
+/**
+ * Insert an item into the feed, either before or after an
+ * existing item.
+ *
+ * Intended for internal use.
+ * 
+ * Arguments:
+ *     item      -- The item contents to insert.
+ *     rel_id    -- The ID of an existing item.
+ *     callback  -- Executed on successful publish.
+ *     placement -- Either 'BEFORE' or 'AFTER', and indicates
+ *                  where the item will be inserted in relation
+ *                  to rel_id.
+ *
+ * Callback Arguments:
+ *     item -- The content of the published item.
+ *     id   -- The generated ID of the item.
+ */
+function sortedFeedPublishInsert(item, rel_id, callback, placement) {
     if(placement == 'BEFORE') {
-        placement = ':' + before_id;
+        placement = ':' + rel_id;
     } else {
-        placement = before_id + ':';
+        placement = rel_id + ':';
     }
     this.mredis.watch('feed.items:' + this.name, function(err, reply) {
-        this.mredis.hexists('feed.items:' + this.name, before_id, function(err, reply) {
+        this.mredis.hexists('feed.items:' + this.name, rel_id, function(err, reply) {
             if(!reply) {
                 this.mredis.unwatch(function(err, reply) {
                     this.thoonk.lock.unlock();
@@ -90,7 +162,7 @@ function sortedFeedPublishInsert(item, before_id, callback, placement) {
             this.mredis.incr('feed.idincr:' + this.name, function(err, reply) {
                 var id = reply;
                 this.mredis.multi()
-                    .linsert('feed.ids:' + this.name, placement, before_id, id)
+                    .linsert('feed.ids:' + this.name, placement, rel_id, id)
                     .hset('feed.items:' + this.name, id, item)
                     .inc('feed.publishes:' + this.name)
                     .publish('feed.publish:' + this.name, id + '\x00' + item)
@@ -99,7 +171,7 @@ function sortedFeedPublishInsert(item, before_id, callback, placement) {
                     this.thoonk.lock.unlock();
                     if(!reply) {
                         process.nexttick(function() {
-                            this.publishInsert(item, before_id, callback, placement);
+                            this.publishInsert(item, rel_id, callback, placement);
                         }.bind(this));
                     } else {
                         if(callback) { callback(item, id); }
@@ -110,15 +182,56 @@ function sortedFeedPublishInsert(item, before_id, callback, placement) {
     }.bind(this));
 }
 
-function sortedFeedPublishBefore(item, after_id, callback) {
-    this.publishInsert(this, item, after_id, callback, 'BEFORE');
+/**
+ * Insert an item immediately before an existing item.
+ * 
+ * Arguments:
+ *     item      -- The item contents to insert.
+ *     before_id -- The ID of an existing item.
+ *     callback  -- Executed on successful publish.
+ *
+ * Callback Arguments:
+ *     item -- The content of the published item.
+ *     id   -- The generated ID of the item.
+ */
+function sortedFeedPublishBefore(item, before_id, callback) {
+    this.publishInsert(this, item, before_id, callback, 'BEFORE');
 }
 
+/**
+ * Insert an item immediately after an existing item.
+ * 
+ * Arguments:
+ *     item      -- The item contents to insert.
+ *     after_id -- The ID of an existing item.
+ *     callback  -- Executed on successful publish.
+ *
+ * Callback Arguments:
+ *     item -- The content of the published item.
+ *     id   -- The generated ID of the item.
+ */
 function sortedFeedPublishAfter(item, after_id, callback) {
-    this.publishInsert(item, after_id, callback, 'AFTER');
+    this.publishInsert(this, item, after_id, callback, 'AFTER');
 }
 
-//callback(err_msg, id, placement);
+/**
+ * Move an existing item to before or after an existing item.
+ * 
+ * The placement paramter specifies where to move the item
+ * in relation to the given relative ID. If the placement
+ * is BEGIN or END, then the item is moved to appropriate
+ * end of the feed.
+ *
+ * Arguments:
+ *     id          -- The ID of the item to move.
+ *     relative_id -- The ID of an existing item.
+ *     placement   -- One of: BEFORE, AFTER, BEGIN, END
+ *
+ * Callback Arguments:
+ *     error     -- A string error message. 
+ *     id        -- The ID of the item to move.
+ *     placement -- The desired placement for the item.
+ */
 function sortedFeedMove(id, relative_id, placement) {
     var relative;
     if(placement == 'BEFORE') {
@@ -176,23 +289,59 @@ function sortedFeedMove(id, relative_id, placement) {
 
 }
 
+/**
+ * Move an item before an existing item.
+ *
+ * Arguments:
+ *     id          -- The ID of the item to move. 
+ *     relative_id -- The ID to move the item before.
+ */
 function sortedFeedMoveBefore(id, relative_id) {
     this.move(id, relative_id, 'BEFORE');
 }
 
+/**
+ * Move an item after an existing item.
+ *
+ * Arguments:
+ *     id          -- The ID of the item to move. 
+ *     relative_id -- The ID to move the item after.
+ */
 function sortedFeedMoveAfter(id, relative_id) {
     this.move(id, relative_id, 'AFTER');
 }
 
+/**
+ * Move an item to the beginning of the feed.
+ *
+ * Arguments:
+ *     id -- The ID of the item to move.
+ */
 function sortedFeedMoveBegin(id) {
     this.move(id, null, 'BEGIN');
 }
 
+/**
+ * Move an item to the end of the feed.
+ *
+ * Arguments:
+ *     id -- The ID of the item to move.
+ */
 function sortedFeedMoveEnd(id) {
     this.move(id, null, 'END');
 }
 
-//callback(id, error_msg);
+/**
+ * Remove an item from the feed.
+ * 
+ * Arguments:
+ *     id       -- The ID value of the item to remove.
+ *     callback -- Executed on successful retraction.
+ *
+ * Callback Arguments:
+ *     id -- ID of the removed item. 
+ *     err_msg -- A string or null.
+ */
 function sortedFeedRetract(id, callback) {
     this.mredis.watch('feed.items:' + this.name, function(err, reply) {
         this.mredis.hexists('feed.items:' + this.name, id, function(err, reply) {
@@ -220,18 +369,49 @@ function sortedFeedRetract(id, callback) {
     }.bind(this));
 }
 
+/**
+ * Return the set of IDs used by items in the feed."""
+ *
+ * Arguments:
+ *     callback -- 
+ *
+ * Callback Arguments:
+ *     error -- A string or null.
+ *     reply -- The Redis reply object.
+ */
 function sortedFeedGetIds(callback) {
     this.mredis.lrange('feed.ids:' + this.name, 0, -1, function(err, reply) {
         callback(reply);
     }.bind(this));
 }
 
+/**
+ * Retrieve a single item from the feed.
+ *
+ * Arguments:
+ *     id       -- The ID of the item to retrieve.
+ *     callback -- 
+ *
+ * Callback Arguments:
+ *     error -- A string or null.
+ *     reply -- The Redis reply object.
+ */
 function sortedFeedGetItem(id, callback) {
     this.mredis.hget('feed.items:' + this.name, id, function(err, reply) {
         callback(reply);
     }.bind(this));
 }
 
+/**
+ * Retrieve all items from the feed.
+ *
+ * Arguments:
+ *     callback -- 
+ *
+ * Callback Arguments:
+ *     error -- A string or null.
+ *     reply -- The Redis reply object.
+ */
 function sortedFeedGetAll(callback) {
     this.mredis.hgetall('feed.items:' + this.name, function(err, reply) {
         callback(reply);
