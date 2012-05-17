75a013afe0d05e40487a42074f07b214bbc5420e
Aaron McCall <aaron@andyet.net>
2012-05-16 23:50:30 -0700

    Retractify!

--- scripts/job/retract.lua
+++ scripts/job/retract.lua
@@ -1,11 +1,11 @@
 -- ARGV: name, id
-if redis.call('hdel', 'job.items:'..ARGV[1], ARGV[2]) == 0 then
-    return false
+local name, id = unpack(ARGV);
+if redis.call('hdel', 'job.items:'..name, id) == 0 then
+    return {'Job not found!', id}
 end
-redis.call('hdel', 'job.cancelled:'..ARGV[1], ARGV[2])
-redis.call('zrem', 'job.published:'..ARGV[1], ARGV[2])
-redis.call('srem', 'job.stalled:'..ARGV[1], ARGV[2])
-redis.call('zrem', 'job.claimed:'..ARGV[1], ARGV[2])
-redis.call('lrem', 'job.ids:'..ARGV[1], 1, ARGV[2])
-return true
-
+redis.call('hdel', 'job.cancelled:'..name, id)
+redis.call('zrem', 'job.published:'..name, id)
+redis.call('srem', 'job.stalled:'..name, id)
+redis.call('zrem', 'job.claimed:'..name, id)
+redis.call('lrem', 'job.ids:'..name, 1, id)
+return {nil, id}
