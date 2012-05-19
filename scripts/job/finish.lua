2f9b9edb3022fdeb8f7ff758c1f22f2ce5a66f66
Aaron McCall <aaron@andyet.net>
2012-05-18 18:01:25 -0700

    corrected job.finish publish payload

--- scripts/job/finish.lua
+++ scripts/job/finish.lua
@@ -7,7 +7,13 @@ end
 redis.call('hdel', 'job.cancelled:'..name, id)
 redis.call('zrem', 'job.published:'..name, id)
 redis.call('incr', 'job.finishes:'..name)
-redis.call('publish', 'job.finish:'..name, id)
+local publish_payload;
+if result then
+    publish_payload = id.."\0"..result
+else
+    publish_payload = id
+end
+redis.call('publish', 'event.job.finish:'..name, publish_payload)
 redis.call('hdel', 'job.items:'..name, id)
 if result then
     return {false, id, result};
