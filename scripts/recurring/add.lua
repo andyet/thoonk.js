33b9d8c101f4efc3c414a0c4d240bf94a4bd0b88
Nathan Fritz <fritzy@netflint.net>
2012-09-27 10:28:39 -0700

    adding a duplicate recurring task no longer pushes the task time out.

--- scripts/recurring/add.lua
+++ scripts/recurring/add.lua
@@ -1,4 +1,9 @@
 local name, task, curtime = unpack(ARGV)
 local timeout = redis.call('HGET', 'recurring.config:'..name, 'timeout');
-redis.call('ZADD', 'recurring:'..name, curtime + timeout, task);
-return {false, task, curtime + timeout}
+local score = redis.call('ZSCORE', 'recurring:'..name, task);
+if(score == false) then
+    redis.call('ZADD', 'recurring:'..name, curtime + timeout, task);
+    return {false, task, curtime + timeout}
+else
+    return {"ALREADY EXISTS", task, score}
+end
